dae8ab2a2d043b73c65c85dd4075ed76a93ab48d
Thomas Grainger <tagrain@gmail.com>
2017-03-06 09:27:12 +0000

    Update trove for Python 3.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     test_suite='nose.collector',
